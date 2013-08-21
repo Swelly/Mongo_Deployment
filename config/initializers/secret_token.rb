11fe0e98c87228c424680c858cd0b00263b9e594
Wesley Carr <surfswell17@gmail.com>
2013-08-21 14:46:14 -0400

    First Mongo app up (books / posts)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongos::Application.config.secret_token = '2e360a81290f4e89292c5db5d51877ee659eb73a5f06bdb965574616bc515800b4d6e7c9f035e5720d3f94d30ca9004dc0b057c0e11ed52859c769dd408c6b2f'
